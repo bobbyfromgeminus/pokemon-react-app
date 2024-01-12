9e9abd01ccfc00d4b5e0c92974e9c7b7c871ae84
bobbyfromgeminus <info.geminus@gmail.com>
2024-01-12 13:18:31 +0100

    location images

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -4,6 +4,86 @@ function Locations({setComponent, setLocation}) {
 
     const [locations, setLocations] = useState([]);
 
+    let landscapes = [
+      {
+        type: 'city',
+        counter: 0
+      },
+      {
+        type: 'league',
+        counter: 0
+      },
+      {
+        type: 'mine',
+        counter: 0
+      },
+      {
+        type: 'windworks',
+        counter: 0
+      },
+      {
+        type: 'forest',
+        counter: 0
+      },
+      {
+        type: 'ironworks',
+        counter: 0
+      },
+      {
+        type: 'coronet',
+        counter: 0
+      },
+      {
+        type: 'marsh',
+        counter: 0
+      },
+      {
+        type: 'ruins',
+        counter: 0
+      },
+      {
+        type: 'road',
+        counter: 0
+      },
+      {
+        type: 'path',
+        counter: 0
+      },
+      {
+        type: 'gate',
+        counter: 0
+      },
+      {
+        type: 'mountain',
+        counter: 0
+      },
+      {
+        type: 'cave',
+        counter: 0
+      },
+      {
+        type: 'temple',
+        counter: 0
+      }
+    ];
+
+    const setBackground = (locations) =>{
+      let locationContainer = locations;
+      locationContainer.forEach( (location) => {
+        landscapes.forEach (landscape => {
+          console.log(location.name, landscape.type);
+          if (location.name.includes(landscape.type)) {
+            landscape.counter++;
+            console.log(`bg_${landscape.type}_${landscape.counter}`);
+            location.cssClass = `bg_${landscape.type}_${landscape.counter}`;
+          }
+        })
+      })
+      setLocation(locationContainer);
+    }
+
+    setBackground(locations);
+
     async function fetchData(url) {
       const response = await fetch(url);
       return response.json();
@@ -33,8 +113,8 @@ function Locations({setComponent, setLocation}) {
             <p>It's time to decide where you want to travel! But I warn you, there are dangers on the way!</p>
             <div className="locations-container">
               {locations.map((location, index) => (
-                  <div onClick={ () => handleClick(location)} key={index}>
-                    {location.name}
+                  <div  className={location.cssClass} onClick={ () => handleClick(location)} key={index} id={location.name}>
+                    <p>{location.name}</p>
                   </div>
               ))}
             </div>
